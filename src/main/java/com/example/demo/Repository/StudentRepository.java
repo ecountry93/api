cd387bcae5b13bb8260bb2e4a7d0f058ddc42204
ecountry93 <145100931+ecountry93@users.noreply.github.com>
2023-10-18 18:26:31 +0300

    Update StudentRepository.java

--- src/main/java/com/example/demo/Repository/StudentRepository.java
+++ src/main/java/com/example/demo/Repository/StudentRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.Entity.Student;
 
-
+//@Repository annotation is used to indicate that the class provides the mechanism for storage, retrieval, search,update and delete operation on objects.
 @Repository
+//Spring Data JPA  - the most compelling feature is the ability to create repository implementations automatically, at runtime, 
+//from a repository interface 
 public interface StudentRepository extends JpaRepository<Student,Long>{
 
 }
